43a3f862fd8cdc04ba5b1f25d83f20703c9724ca
Mei Wu <meiwu@utexas.edu>
2014-10-19 01:24:28 -0500

    *

--- product.js
+++ product.js
@@ -8,41 +8,39 @@ function setSize(){
     var textResult= $("#textResult");
     var size = document.forms["textAwesome"]["textSize"].value; 
     textResult.css("font-size", size + "px");
-    document.getElementById("textResult").innerHTML = textToAwesome;
 }
 
 function setColor(){
     var textResult= $("#textResult");
     var textColor = document.forms["textAwesome"]["color"].value;
     textResult.css("color", textColor);
-    document.getElementById("textResult").innerHTML = textToAwesome;
 }
 
 function setFont(){
     var textResult= $("#textResult");
     var font = document.forms["textAwesome"]["font"].value;
     textResult.css("font-family", font);
-    document.getElementById("textResult").innerHTML = textToAwesome;
 }
 
 function setCapitalization(){
+	var textToAwesome = document.forms["textAwesome"]["input"].value;
 	var capitalization = document.forms["textAwesome"]["capitalization"].value;
 	var newText = "";
-	if(capitalization.equals("uppercase")){
-		newText += capitalization.toUpperCase();
-	}else if(capitalization.equals("lowercase")){
-		newText += capitalization.toLowerCase();
+	if(capitalization === "uppercase"){
+		newText += textToAwesome.toUpperCase();
+	}else if(capitalization === "lowercase"){
+		newText += textToAwesome.toLowerCase();
 	}else{
-		var textLength = capitalization.length();
-		for (var index = 0; index < capitalization.length; index++) {
+		for (var index = 0; index < textToAwesome.length; index++) {
 			var capitalize = Math.random() < .5;
 			if(capitalize)
-				newText += capitalization.charAt(index).toUpperCase();
+				newText += textToAwesome.charAt(index).toUpperCase();
 			else
-				newText += capitalization.charAt(index).toLowerCase();
+				newText += textToAwesome.charAt(index).toLowerCase();
 		}
 	}
-	
+	$("input").val(newText);
+	setText();
 	
 }
 	
